21f73b092548d9dc55160e44c6e77e61673649f0
Ney Hernandez <nher1625@gmail.com>
2020-05-23 12:53:30 -0400

    remove withCredentials

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default function () {
   return axios.create({
     baseURL: process.env.VUE_APP_API_BASE,
-    withCredentials: true,
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
