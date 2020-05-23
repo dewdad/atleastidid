98e2eb3cdc5c50d4cdafda9a76768ea44ec7eb03
Ney Hernandez <nher1625@gmail.com>
2020-05-23 13:19:45 -0400

    axios headers

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 var axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_BASE
 })
-axiosInstance.headers.common['Access-Control-Allow-Origin'] = '*'
+axiosInstance.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 export default function () {
   return axiosInstance
 }
