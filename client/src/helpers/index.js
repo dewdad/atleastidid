a60b3f097e8f7dac6c3c0816f3052018c3f1413d
Ney Hernandez <nher1625@gmail.com>
2020-05-26 22:49:49 -0400

    Simplify View route page titles...

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -3,6 +3,9 @@ import store from '@/store'
 var helpers = {
   loggedIn: function () {
     return store.getters['auth/loggedIn']
+  },
+  getPathName (path) {
+    return path.slice(1, path.length)
   }
 }
 
