d60fa01c758958964a1984376ba3ce08b1c57297
Ney Hernandez <nher1625@gmail.com>
2020-05-27 00:33:00 -0400

    Format this whole file with visual studio default formatter hotkey settings...

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -1,46 +1,59 @@
 <template>
   <div id="task-list-component" class="container-fluid py-5">
-    <span>All of your <b>Latest Tasks</b></span>
+    <span>
+      All of your
+      <b>Latest Tasks</b>
+    </span>
     <div v-if="hasTasks && !loading" class="row flex">
       <template v-if="hasTasks">
-      <div class="col-12">
-        <ul class="list-group">
-          <li
-            v-for="task in tasks" 
-            :key="task.id" 
-            :class="`list-group-item task-${task.id}`"
-            :data-id="task.id">
-            <div class="row position-relative">
-              <div class="col">
-                <div class="title">
-                  <b>Title:</b> {{ task.title }}
+        <div class="col-12">
+          <ul class="list-group">
+            <li
+              v-for="task in tasks"
+              :key="task.id"
+              :class="`list-group-item task-${task.id}`"
+              :data-id="task.id"
+            >
+              <div class="row position-relative">
+                <div class="col">
+                  <div class="title">
+                    <b>Title:</b>
+                    {{ task.title }}
+                  </div>
+                  <div class="content">
+                    <b>Body:</b>
+                    {{ task.body }}
+                  </div>
                 </div>
-                <div class="content">
-                  <b>Body:</b> {{ task.body }}
+                <div class="col">
+                  <p v-if="completedTime">
+                    <b>Completed:</b>
+                    {{ completedTime.split(',')[0] + " | " + completedTime.split(',')[1] }}
+                  </p>
+                  <p>
+                    <b>Created:</b>
+                    {{ task.createdAt | dateCreated }}
+                  </p>
+                </div>
+                <div class="col">
+                  <a
+                    href="#"
+                    class="btn btn-danger position-absolute"
+                    style="right: 0; top: 0;"
+                    @click.prevent="deleteTask(task.id)"
+                  >Delete</a>
+                  <TaskCheckbox :task="task" @taskMarked="taskWasMarked" />
                 </div>
               </div>
-              <div class="col">
-                <p v-if="completedTime"><b>Completed:</b> {{ completedTime.split(',')[0] +  " | " + completedTime.split(',')[1] }}</p>
-                <p><b>Created:</b> {{ createdAtFormat(task.createdAt) }}</p>
-              </div>
-              <div class="col">
-                <a
-                  href="#"
-                  class="btn btn-danger position-absolute"
-                  style="right: 0; top: 0;"
-                  @click.prevent="deleteTask(task.id)">Delete</a>
-                  <TaskCheckbox :task="task" @taskMarked="taskWasMarked(task)" />
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+            </li>
+          </ul>
+        </div>
       </template>
       <template v-else>
         <div class="alert alert-default w-100">
           <p class="text-center">You haven't shared any tasks.</p>
           <a
-            href=""
+            href
             @click.prevent="$router.push({ name: 'create-task' })"
             class="btn btn-primary"
           >Create a Task</a>
@@ -48,10 +61,8 @@
       </template>
     </div>
     <div v-else>
-      <div class="alert alert-danger mt-1">
-        You haven't created a task yet :(
-      </div>
-      <router-link tag="a" to="/add-task">Click here</router-link> to create your first task!
+      <div class="alert alert-danger mt-1">You haven't created a task yet :(</div>
+      <router-link tag="a" to="/add-task">Click here</router-link>to create your first task!
     </div>
     <div v-if="loading" class="row text-center">
       <div class="py-5 w-100 position-relative">
@@ -79,32 +90,28 @@ export default {
   },
   computed: {
     hasTasks() {
-      if (this.tasks && this.tasks.length) return true
-      return false
+      if (this.tasks && this.tasks.length) return true;
+      return false;
     }
   },
   methods: {
     taskWasMarked(task) {
-      var el 
-      if (task.id) el = document.querySelector(`.task-${task.id}`)
-      window.console.log(el)
-      if (el.classList.contains('completed-task') && !this.completed) {
-        this.completedTime = null
-        el.classList.remove('completed-task')
-        return
+      window.console.log(task)
+      if (task) {
+        let taskEl = window.document.querySelector(`.task-${task.id}`)
+        if (task.completed) {
+          taskEl.classList.add('completed-task')
+        } else {
+          taskEl.classList.remove('completed-task')
+        }
       }
-
-      if (task.completed && task.id) {
-        this.completedTime = new Date().toLocaleString()
-        el.className = (`list-group-item task-${task.id} completed-task`)
-      } 
     },
     async fetchAllTasks() {
       try {
         let response = await TasksService.getAllTasks();
         if (response.status === 200) {
-          this.tasks = response.data.tasks
-          this.loading = false
+          this.tasks = response.data.tasks;
+          this.loading = false;
         }
       } catch (err) {
         window.console.error(err);
@@ -114,17 +121,11 @@ export default {
       try {
         let response = await TasksService.deleteTask(id);
         if (response.status === 200) {
-          await this.fetchAllTasks()
+          await this.fetchAllTasks();
         }
       } catch (err) {
         window.console.error(err);
       }
-    },
-    createdAtFormat(dateStr) {
-      let d = new Date(dateStr);
-      let date = d.toLocaleDateString();
-      let time = d.toLocaleTimeString();
-      return `${date} | ${time}`;
     }
   },
   mounted() {
@@ -134,12 +135,12 @@ export default {
 </script>
 
 <style scoped>
-  .completed-task .title,
-  .completed-task .content {
-    text-decoration: line-through;
-  }
-  .spinner-border.text-primary.position-absolute {
-    left: calc(50% - 20px);
-    top: 0px;
-  }
+.completed-task .title,
+.completed-task .content {
+  text-decoration: line-through;
+}
+.spinner-border.text-primary.position-absolute {
+  left: calc(50% - 20px);
+  top: 0px;
+}
 </style>
