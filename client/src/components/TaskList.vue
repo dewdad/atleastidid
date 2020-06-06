e082bd5bde0a3c7096dc865f2ff42042f5259ac7
Ney Hernandez <nher1625@gmail.com>
2020-06-06 13:04:01 -0400

    Frontend edits for Task List component.

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -15,34 +15,45 @@
               :data-id="task.id"
             >
               <div class="row position-relative">
-                <div class="col">
-                  <div class="title">
+                <div class="col d-flex align-items-center">
+                  <div class="title mb-0">
                     <b>Title:</b>
                     {{ task.title }}
                   </div>
-                  <div class="content">
+                  <!-- <div class="content">
                     <b>Body:</b>
                     {{ task.body }}
-                  </div>
+                  </div> -->
                 </div>
-                <div class="col">
-                  <p v-if="completedTime">
+                <div class="col d-flex align-items-center">
+                  <p v-if="completedTime" class="mb-0">
                     <b>Completed:</b>
                     {{ completedTime.split(',')[0] + " | " + completedTime.split(',')[1] }}
                   </p>
-                  <p>
+                  <p class="mb-0">
                     <b>Created:</b>
                     {{ task.createdAt | dateCreated }}
                   </p>
                 </div>
-                <div class="col">
-                  <a
-                    href="#"
-                    class="btn btn-danger position-absolute"
-                    style="right: 0; top: 0;"
-                    @click.prevent="deleteTask(task.id)"
-                  >Delete</a>
+                <div class="col completion d-flex align-items-center">
                   <TaskCheckbox :task="task" @taskMarked="taskWasMarked" />
+                </div>
+                <div class="col actions">
+                  <div class="btn-group" role="group" aria-label="Basic example">
+                    <router-link
+                      tag="a"
+                      class="btn btn-primary"
+                      :to="`/task/${task.id}`">
+                      👁️
+                    </router-link>
+                    <button type="button" class="btn btn-primary">Middle</button>
+                    <a
+                      href="#"
+                      class="btn btn-primary"
+                      @click.prevent="deleteTask(task.id)">
+                      🗑️
+                    </a>
+                  </div>
                 </div>
               </div>
             </li>
@@ -53,16 +64,15 @@
         <div class="alert alert-default w-100">
           <p class="text-center">You haven't shared any tasks.</p>
           <a
-            href
+            href 
             @click.prevent="$router.push({ name: 'create-task' })"
-            class="btn btn-primary"
-          >Create a Task</a>
+            class="btn btn-primary">Create a Task</a>
         </div>
       </template>
     </div>
     <div v-else>
       <div class="alert alert-danger mt-1">You haven't created a task yet :(</div>
-      <router-link tag="a" to="/add-task">Click here</router-link>to create your first task!
+      <router-link tag="a" to="/add-task">Click here</router-link> to create your first task!
     </div>
     <div v-if="loading" class="row text-center">
       <div class="py-5 w-100 position-relative">
@@ -85,19 +95,17 @@ export default {
       tasks: []
     };
   },
-  components: {
-    TaskCheckbox
-  },
+  components: { TaskCheckbox },
   computed: {
     hasTasks() {
-      if (this.tasks && this.tasks.length) return true;
+      if (this.tasks.length > 0) return true;
       return false;
     }
   },
   methods: {
     taskWasMarked(task) {
-      window.console.log(task)
       if (task) {
+        window.console.log(task)
         let taskEl = window.document.querySelector(`.task-${task.id}`)
         if (task.completed) {
           taskEl.classList.add('completed-task')
@@ -109,7 +117,7 @@ export default {
     async fetchAllTasks() {
       try {
         let response = await TasksService.getAllTasks();
-        if (response.status === 200) {
+        if (response.status == 200) {
           this.tasks = response.data.tasks;
           this.loading = false;
         }
@@ -120,7 +128,7 @@ export default {
     async deleteTask(id) {
       try {
         let response = await TasksService.deleteTask(id);
-        if (response.status === 200) {
+        if (response.status == 200) {
           await this.fetchAllTasks();
         }
       } catch (err) {
