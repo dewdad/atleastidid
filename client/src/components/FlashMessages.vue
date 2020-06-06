ef07ab5d895ea9cf845145d861a3c63dacbcc8b1
Ney Hernandez <nher1625@gmail.com>
2020-06-06 12:59:58 -0400

    Add the dismissable alert close button element and make props one line.

--- client/src/components/FlashMessages.vue
+++ client/src/components/FlashMessages.vue
@@ -1,9 +1,14 @@
 <template>
-  <div 
+  <div
+    v-if="notice"
     id="flash-messages-component"
     class="alert alert-primary alert-dismissible fade show position-absolute"
     role="alert">
     <strong>{{ notice.message }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <!-- <pre>{{notice}}</pre> -->
   </div>
 </template>
 
@@ -11,10 +16,7 @@
 export default {
   name: "flash-messages",
   props: {
-    notice: {
-      type: Object,
-      required: true
-    }
+    notice: { type: Object }
   },
   watch: {
     notice () {
