38d054700e6e58312c164b956b426ddcce6c72b3
nher1625 <nher1625@gmail.com>
2020-06-10 19:59:02 -0400

    Switch to *

--- server/src/index.js
+++ server/src/index.js
@@ -40,7 +40,7 @@ app.use(errorhandler())
 app.use(morgan('common'))
 if (environment == 'production') {
   var corsOptions = {
-    origin: 'atleastidid.com',
+    origin: '*',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true,
