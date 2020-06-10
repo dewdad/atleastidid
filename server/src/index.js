22abe7872f14a5ed4f2fedb361277392f8d38f96
nher1625 <nher1625@gmail.com>
2020-06-10 18:17:05 -0400

    Change origin to reflect what frontend uses in headers

--- server/src/index.js
+++ server/src/index.js
@@ -40,7 +40,7 @@ app.use(errorhandler())
 app.use(morgan('common'))
 if (environment == 'production') {
   var corsOptions = {
-    origin: ['https://198.211.96.108:4000'],
+    origin: ['https://atleastidid.com'],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true,
