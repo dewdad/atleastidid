a7b20c89ee97cb4f7b4fe6b7a3d5e4c814baaf59
nher1625 <nher1625@gmail.com>
2020-06-10 18:02:15 -0400

    Try using ip with https protocol in allowed origins whitelist

--- server/src/index.js
+++ server/src/index.js
@@ -40,7 +40,7 @@ app.use(errorhandler())
 app.use(morgan('common'))
 if (environment == 'production') {
   var corsOptions = {
-    origin: ['atleastidid.com', 'www.atleastidid.com'],
+    origin: ['https://198.211.96.108:4000', 'atleastidid.com', 'www.atleastidid.com'],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true,
