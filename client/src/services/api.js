c7cbb40a458b07a344ebe07be5cc16933c87f979
nher1625 <nher1625@gmail.com>
2020-06-10 18:00:02 -0400

    Undo this change that was ok

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ var axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_BASE,
   withCredentials: true
 })
-axiosInstance.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+//axiosInstance.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 export default function () {
   return axiosInstance
 }
