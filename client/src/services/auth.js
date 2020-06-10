7baa277fc2a35923160e6daa7715dd0c76d2f0ec
nher1625 <nher1625@gmail.com>
2020-06-10 19:54:17 -0400

    Test adding crossDomain config to post request

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -5,7 +5,7 @@ export default {
     return Api().post('register', credentials)
   },
   login (credentials) {
-    return Api().post('login', credentials)
+    return Api().post('login', credentials, { crossDomain: true })
   },
   logout () {
     return Api().post('logout')
