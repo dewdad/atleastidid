b512518525b0e72037866e616d05f83b96299276
nher1625 <nher1625@gmail.com>
2020-06-10 18:18:13 -0400

    ...

--- server/src/index.js
+++ server/src/index.js
@@ -40,7 +40,7 @@ app.use(errorhandler())
 app.use(morgan('common'))
 if (environment == 'production') {
   var corsOptions = {
-    origin: ['https://atleastidid.com'],
+    origin: ['https://atleastidid.com', 'https://198.211.96.108:4000/api/v1/login'],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true,
