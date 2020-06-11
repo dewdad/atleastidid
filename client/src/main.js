5aa3ea2195d20ad8330480bbc401a9c145939bcb
nher1625 <nher1625@gmail.com>
2020-06-11 11:56:48 -0400

    Minor adjustment to how styles are loaded so we don't lose any on page refreshes.

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import helpers from './helpers'
+import '@/assets/style.css'
 
 Vue.config.productionTip = false
 
